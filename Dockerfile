f5ed9daf8639e05679ab579791e49554b93de55d
aoyagi <ckg24080@st.ckg.ac.jp>
2026-02-19 13:30:42 +0900

    土日と祝日の色分け

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
+#（超重要）Dockerfileを書き換えた時はdockerをビルドしなおす
+
+
 #ベースとなるPythonが入ったOSを選ぶ
 FROM python:3.11-slim
 
@@ -5,7 +8,8 @@ FROM python:3.11-slim
 WORKDIR /app
 
 #必要な道具をインストールする
-RUN pip install fastapi uvicorn jinja2
+RUN pip install fastapi uvicorn jinja2 holidays
+#                                       ↑祝日判定ライブラリ
 
 #自分のパソコンにあるファイルを箱の中（コンテナ）にコピーする
 COPY . .
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 import calendar
+import holidays
 
 #FastAPI の app(アプリの本体) を作る
 app = FastAPI()
@@ -11,7 +12,8 @@ templates = Jinja2Templates(directory="templates")
 #ブラウザでトップページ(/)にアクセスしたときの動きを決める
 @app.get("/")
 def read_root(request: Request):
-    #2026年2月のカレンダーを生成
+    jp_holidays = holidays.Japan(years=2026)
+    
     #monthcalendar(年,月)は1週間ごとのリストを返す。その月に含まれていない日(今回の場合3/1等)は 0 として帰ってくる
     cal = calendar.monthcalendar(2026, 2)
     
@@ -22,5 +24,6 @@ def read_root(request: Request):
         "request": request,
         "title": "2026年 2月",
         "week_days": week_days,
-        "cal": cal # 2次元のリスト（リストの中にリストが入っている状態）を送る
+        "cal": cal, # 2次元のリスト（リストの中にリストが入っている状態）を送る
+        "jp_holidays": jp_holidays #祝日データを送る
     })
